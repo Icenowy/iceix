6270afb1560a50b5ff87cdd51ba4ce3941147ca1
Icenowy Zheng <icenowy@outlook.com>
2015-12-24 23:00:27 +0800

    remove __ in portio.h

--- include/arch/portio.h
+++ include/arch/portio.h
@@ -3,21 +3,21 @@
 
 #define inb(port) (__extension__({	\
 unsigned char __res;	\
-__asm__ volatile ("inb	%%dx,	%%al\n\t"	\
+asm volatile ("inb	%%dx,	%%al\n\t"	\
 					 :"=a"(__res)	\
 					 :"dx"(port));	\
 __res;	\
 }))
 
-#define outb(value, port) __asm__ volatile (	\
+#define outb(value, port) asm volatile (	\
 "outb	%%al,	%%dx\n\t"::"al"(value), "dx"(port))
 
 #define insl(port, buf, nr) \
-__asm__ volatile ("cld;rep;insl\n\t"	\
+asm volatile ("cld;rep;insl\n\t"	\
 ::"d"(port), "D"(buf), "c"(nr))
 
 #define outsl(buf, nr, port) \
-__asm__ volatile ("cld;rep;outsl\n\t"	\
+asm volatile ("cld;rep;outsl\n\t"	\
 ::"d"(port), "S" (buf), "c" (nr))
 
 #endif
