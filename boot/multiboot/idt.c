ada6a91e885c8796fa33fc6bc89034819a94c4ee
Icenowy Zheng <icenowy@outlook.com>
2015-12-25 11:09:14 +0800

    sti now works!

--- boot/multiboot/idt.c
+++ boot/multiboot/idt.c
@@ -1,4 +1,5 @@
 #include <arch/idt.h>
+#include <arch/address.h>
 #include <stdbool.h>
 
 #define IDT_LENGTH 256
@@ -20,12 +21,14 @@ void init_idt_item (uint8_t n, void (*function) ())
 	idt_gates[n].offset_l = (uint16_t) (uifp & 0xFFFF);
 	idt_gates[n].offset_h = (uint16_t) (uifp >> 16);
 	idt_gates[n].reserved = 0;
+	idt_gates[n].always = 0;
 	idt_gates[n].present = 1;
 	for (unsigned i = 0; i< (sizeof(traps) / sizeof (uint8_t)); i++) {
 		if (traps[i] == n) is_trap = true;
 	}
-	idt_gates[n].type = is_trap ? 7 : 6;
+	idt_gates[n].type = is_trap ? 0xF : 0xE;
 	idt_gates[n].dpl = 0;
+	idt_gates[n].selector = KERNEL_CODE_SEL;
 }
 
 void init_idt ()
@@ -33,4 +36,5 @@ void init_idt ()
 	for (int i = 0; i<IDT_LENGTH; i++) {
 		init_idt_item (i, * ( (&vectors) + i));
 	}
+	install_idt ();
 }
--- include/arch/int.h
+++ include/arch/int.h
@@ -2,6 +2,6 @@
 #define _ARCH_INT_H
 
 #define mask_interrupt() __asm__ ("cli\n\t")
-#define allow_interrupt() __asm__ ("cli\n\t")
+#define allow_interrupt() __asm__ ("sti\n\t")
 
 #endif
