3afe927f1ab5366e4bba2ea6e356e6d6d4c4643b
Icenowy Zheng <icenowy@outlook.com>
2015-12-25 16:13:31 +0800

    fixed jitter in timer demo

--- barebone/kmain.c
+++ barebone/kmain.c
@@ -9,7 +9,7 @@ void kmain()
 	vtty_write ("Merry Christmas!\n", 17);
 	current_jiffies = 0;
 	for (;;) {
-		if ((! (jiffies % HZ)) && jiffies > current_jiffies) {
+		if ((! (jiffies % HZ)) && (jiffies - current_jiffies) >= HZ) {
 			vtty_write (".", 1);
 			current_jiffies = jiffies;
 		}
